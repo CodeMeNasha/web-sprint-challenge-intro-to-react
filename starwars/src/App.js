ec486366e667e18109ddc7eb9020169745de8bc2
QDG <nasha@QDGs-MacBook-Air.local>
2020-08-01 11:11:24 -0700

    adding effect hook

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  
+  const [people, setPeoplle] = useState();
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/")
+      .then
+      .catch
+  }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -17,3 +25,6 @@ const App = () => {
 }
 
 export default App;
+
+
+// 
