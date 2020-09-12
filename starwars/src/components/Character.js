e2d90d2423fb92f608fa9e236af89b9222b16924
QDG <nasha@QDGs-MacBook-Air.local>
2020-09-12 09:53:51 -0700

    updated styled components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,47 +1,44 @@
 // Write your Character component here
-import React from 'react';
-import styled from 'styled-components';
-export default function Character (props) {
+import React from "react";
+import styled from "styled-components";
+export default function Character(props) {
+  const { charactersData } = props;
+  if (!charactersData) {
+    console.log("Now Loading...");
+    return <h1>Loading...</h1>;
+  }
 
-const { charactersData } = props
-if (!charactersData) {
-    console.log('Now Loading...')
-    return <h1>Loading...</h1>
-}
-
-
+  return charactersData.map((singleCharacter) => {
     return (
-        charactersData.map( singleCharacter => {
-            return (
-            <div className='characterContainer'>
-            <NameH1>{singleCharacter.name}</NameH1>
-            <MainImg src={singleCharacter.image} alt={singleCharacter.species}></MainImg>
-            
-            <TagH3>Status: {singleCharacter.status}</TagH3>
-            <TagH3>Species: {singleCharacter.species}</TagH3>
-            
-            </div>
-        )
-    })
-    )
+      <StyledDiv className="characterContainer">
+        <h1>{singleCharacter.name}</h1>
+        <img src={singleCharacter.image} alt={singleCharacter.species}></img>
+
+        <h3>Status: {singleCharacter.status}</h3>
+        <h3>Species: {singleCharacter.species}</h3>
+      </StyledDiv>
+    );
+  });
 }
 
-const NameH1 = styled.h1`
-font-family: serif;
-font-size: 2rem;
-text-align: center;
-text-decoration: underline;
+const StyledDiv = styled.div`
+  h1 {
+    font-family: serif;
+    font-size: 2rem;
+    text-align: center;
+    text-decoration: underline;
+  }
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    border: 4px solid black;
+  }
+
+  h3 {
+    font-size: 1rem;
+    font-style: italic;
+    text-align: center;
+  }
 `;
 
-const TagH3 = styled.h3`
-font-size: 1rem;
-font-style: italic;
-text-align: center;
-`;
-
-const MainImg = styled.img`
-display: block;
-margin-left: auto;
-margin-right: auto;
-border: 4px solid black;
-`;
